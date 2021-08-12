ee69e34f84ece5be12df075aa2f86f04a45ea87a
tnaseef <79242169+tnaseef@users.noreply.github.com>
2021-08-11 18:21:13 -0600

    HW 19 - jQuery Accordions basics
    + added resume download on index
    + added accordion interaction to lists on skill pg
    - removed red outlines for testing

--- js/index.js
+++ js/index.js
@@ -1,34 +1,55 @@
 console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(function(){ //Empathy
+    $("#flipE").click(function(){
+      $("#panelE").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){ //Creativity
+    $("#flipC").click(function(){
+      $("#panelC").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){ //Tools
+    $("#flipT").click(function(){
+      $("#panelT").slideDown("slow");
+    });
+});
+
+//MODAL
+
 // Get the modal
-var modal = document.getElementById("contactForm");
+//var modal = document.getElementById("contactForm");
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("contactButton");
+//var btn = document.getElementsByClassName("contactButton");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+//var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn[0].onclick = function() {
-  modal.style.display = "block";
-}
+//btn[0].onclick = function() {
+//  modal.style.display = "block";
+//}
 
-btn[1].onclick = function() {
-    modal.style.display = "block";
-}
+//btn[1].onclick = function() {
+//    modal.style.display = "block";
+//}
 
-btn[2].onclick = function() {
-    modal.style.display = "block";
-}
+//btn[2].onclick = function() {
+//    modal.style.display = "block";
+//}
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+//span.onclick = function() {
+//  modal.style.display = "none";
+//}
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+//window.onclick = function(event) {
+//  if (event.target == modal) {
+//    modal.style.display = "none";
+//  }
+//}
